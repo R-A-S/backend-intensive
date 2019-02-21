1914c469ba5fbc60e7b71a96cfc23d077e08abd0
Andrey Prisniak <aprisniak@lectrum.io>
2019-02-21 22:12:12 +0200

    feat(source): add middlewares

--- source/domains/teachers/router.js
+++ source/domains/teachers/router.js
@@ -5,10 +5,14 @@ import express from 'express';
 import * as teachers from './';
 import * as subjects from './subjects';
 
+// Instruments
+import createTeacher from './_schemas/createTeacher';
+import { validator, limiter } from '../../helpers';
+
 const route = express.Router();
 
 route.get('/', teachers.get);
-route.post('/', teachers.post);
+route.post('/', [ limiter(1000, 60 * 1000), validator(createTeacher) ], teachers.post);
 
 route.get('/:teacherId/subjects', subjects.get);
 route.post('/:teacherId/subjects', subjects.post);
--- source/server.js
+++ source/server.js
@@ -6,6 +6,12 @@ import * as domains from './domains';
 
 const app = express();
 
+app.use(
+    express.json({
+        limit: '10kb',
+    }),
+);
+
 app.use('/api/teachers', domains.teachers);
 app.use('/api/pupils', domains.pupils);
 app.use('/api/parents', domains.parents);
