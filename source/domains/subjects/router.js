0151a53f3dda28931bf43933434435a0b730e28d
Andrey Prisniak <aprisniak@lectrum.io>
2019-02-22 22:36:34 +0200

    feat(source): fix subjects routes

--- source/domains/subjects/router.js
+++ source/domains/subjects/router.js
@@ -10,25 +10,25 @@ import * as lesson from './seasons/lessons/lesson';
 
 const route = express.Router();
 
-route.get('/subjects/:subjectsId', subject.get);
-route.post('/subjects/:subjectsId', subject.post);
-route.put('/subjects/:subjectsId', subject.put);
-route.delete('/subjects/:subjectsId', subject.remove);
+route.get('/:subjectsId', subject.get);
+route.post('/:subjectsId', subject.post);
+route.put('/:subjectsId', subject.put);
+route.delete('/:subjectsId', subject.remove);
 
-route.get('/subjects/:subjectsId/seasons', seasons.get);
-route.post('/subjects/:subjectsId/seasons', seasons.post);
+route.get('/:subjectsId/seasons', seasons.get);
+route.post('/:subjectsId/seasons', seasons.post);
 
-route.get('/subjects/:subjectsId/seasons/:seasonId', season.get);
-route.post('/subjects/:subjectsId/seasons/:seasonId', season.post);
-route.put('/subjects/:subjectsId/seasons/:seasonId', season.put);
-route.delete('/subjects/:subjectsId/seasons/:seasonId', season.remove);
+route.get('/:subjectsId/seasons/:seasonId', season.get);
+route.post('/:subjectsId/seasons/:seasonId', season.post);
+route.put('/:subjectsId/seasons/:seasonId', season.put);
+route.delete('/:subjectsId/seasons/:seasonId', season.remove);
 
-route.get('/subjects/:subjectsId/seasons/:seasonId/lessons', lessons.get);
-route.post('/subjects/:subjectsId/seasons/:seasonId/lessons', lessons.post);
+route.get('/:subjectsId/seasons/:seasonId/lessons', lessons.get);
+route.post('/:subjectsId/seasons/:seasonId/lessons', lessons.post);
 
-route.get('/subjects/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.get);
-route.post('/subjects/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.post);
-route.put('/subjects/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.put);
-route.delete('/subjects/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.remove);
+route.get('/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.get);
+route.post('/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.post);
+route.put('/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.put);
+route.delete('/:subjectsId/seasons/:seasonId/lessons/:lessonId', lesson.remove);
 
 export { route as subjects };
