cfa96cc00be318024a67a2c97dc4e2ba73fa36df
R-A-S <r.a.s@live.ru>
2019-03-25 01:46:23 +0200

    hw-18 write

--- source/db/write.js
+++ source/db/write.js
@@ -6,7 +6,6 @@ db;
 
 show collections
 db.customers.drop();
-db.orders.drop();
 
 const customers = [];
 for (let i = 0; i < 10; i++) {
@@ -15,33 +14,13 @@ for (let i = 0; i < 10; i++) {
             first: faker.fName(),
             last:  faker.lName(),
         },
-        balance: Math.floor(Math.random() * 10000),
-        created: new Date().toString(),
+        nickname: `${faker.fName()}_${faker.lName()}'_nickname'`,
+        email:    `${faker.fName()}${faker.lName()}@mail.com`,
+        password: `${Math.floor(Math.random() * 10000000)}${faker.lName()}`,
+        created:  new Date(Date.now() - Math.random() * 1000000000).toString(),
     });
 }
-const { insertedIds } = db.customers.insertMany(customers);
-const customersIds = insertedIds.map((id) => id.valueOf());
-const orders = [];
 
-customersIds.forEach((id) => {
-
-    const ordersCount = Math.floor(Math.random() * (10 - 1) + 1);
-    for (let j = 0; j < ordersCount; j++) {
-        orders.push({
-            customerId: id,
-            count:      parseInt(ordersCount / 2 + 1),
-            price:      Math.floor(Math.random() * (100 - 20) + 20),
-            discount:   Math.floor(Math.random() * (30 - 5) + 5),
-            title:      'Category' + Math.floor(Math.random() * 10),
-            product:    faker.product(),
-        });
-    }
-});
-
-db.orders.insert(orders);
+db.customers.insert(customers);
 
 print('-----------------<<<<<<<<< DONE >>>>>>>>>>------------------');
-print('Orders count :', db.orders.count());
-print('Customers size :', db.customers.dataSize() / 1024, 'KB');
-print('Orders size :', db.orders.dataSize() / 1024, 'KB');
-print('Total size :', db.customers.dataSize() / 1024 + db.orders.dataSize() / 1024, 'KB');
