395e221512639637fbfb2b99adaf7675237fb687
R-A-S <r.a.s@live.ru>
2019-03-31 17:27:31 +0300

    update

--- source/odm/logs.js
+++ source/odm/logs.js
@@ -3,20 +3,23 @@ import mongoose from 'mongoose';
 // Document shape
 const schema = new mongoose.Schema(
     {
-        method:   { type: String },
-        path:     { type: String },
+        method:   String,
+        path:     String,
         duration: {
-            start: { type: Date },
-            end:   { type: Date },
+            start:   Date,
+            endtype: Date,
         },
-        payload: { type: Object },
-        agent:   { type: String },
+        payload: Object,
+        agent:   String,
     },
 
-    { timestamps: { createdAt: 'created' }, capped: { size: 5e7, max: 50000 } },
+    // { timestamps: { createdAt: 'created' }, capped: { size: 5e7, max: 50000 } },
+    { timestamps: { createdAt: 'created' }, capped: { size: 50, max: 1 } },
 );
 
-schema.index({ created: 1, expireAfterSeconds: 60 * 60 * 24 * 7 });
+// schema.index({ created: 1}, {expireAfterSeconds: 60 * 60 * 24 * 7 });
+schema.index({ created: 1 }, { expireAfterSeconds: 20 });
 
 // Collection
 export const logs = mongoose.model('logs', schema);
+logs.ensureIndexes();
