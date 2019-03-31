f928bc433d22210c67317ccc7a204a3ae690ebfd
R-A-S <r.a.s@live.ru>
2019-03-31 18:39:25 +0300

    done

--- source/odm/logs.js
+++ source/odm/logs.js
@@ -13,13 +13,8 @@ const schema = new mongoose.Schema(
         agent:   String,
     },
 
-    // { timestamps: { createdAt: 'created' }, capped: { size: 5e7, max: 50000 } },
-    { timestamps: { createdAt: 'created' }, capped: { size: 50, max: 1 } },
+    { timestamps: { createdAt: 'created' }, capped: { size: 5e7, max: 50000 } },
 );
-
-// schema.index({ created: 1}, {expireAfterSeconds: 60 * 60 * 24 * 7 });
-schema.index({ created: 1 }, { expireAfterSeconds: 20 });
 
 // Collection
 export const logs = mongoose.model('logs', schema);
-logs.ensureIndexes();
